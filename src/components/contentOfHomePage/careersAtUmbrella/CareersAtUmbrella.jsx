49c579d13edd95337e7f62767d723ded35e1c68a
Bazyshyn Anton <maybebazishin@gmail.com>
2023-11-16 17:09:07 +0200

    commit-10 added responsiveness

--- src/components/contentOfHomePage/careersAtUmbrella/CareersAtUmbrella.jsx
+++ src/components/contentOfHomePage/careersAtUmbrella/CareersAtUmbrella.jsx
@@ -1,66 +1,138 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useMediaQuery } from '@mui/material'
 import YellowButton from '../yellowButton/YellowButton'
 import redbook from '../../../pictures/redbook.png'
 const CareersAtUmbrella = () => {
-  return (
-    <Box
-      sx={{
-        position: 'relative',
-        marginTop: '60px',
-        width: '100%',
-        height: '600px',
-      }}>
-      <img
-        style={{
-          objectFit: 'cover',
-          height: '100%',
-          width: '100%',
-          position: 'absolute',
-          left: '-150px',
-        }}
-        src={redbook}
-        alt="pic"
-      />
+  const isSmallScreen = useMediaQuery('(min-width: 1200px)')
 
-      <Box
-        sx={{
-          position: 'absolute',
-          top: '18%',
-          right: '0',
-          background: '#fff',
-          width: '600px',
-          height: '400px',
-          padding: '2pc',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          gap: '1rem',
-        }}>
+  return (
+    <>
+      {isSmallScreen ? (
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            gap: '2rem',
+            position: 'relative',
+            marginTop: '60px',
+            width: '100%',
+            height: '600px',
           }}>
-          <Typography
-            variant="h3"
-            sx={{ fontFamily: "'Open Sans', sans-serif", fontWeight: '600' }}>
-            Careers at Umbrella
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ fontFamily: "'Open Sans', sans-serif", fontWeight: '400' }}>
-            Our mission is clear: translating science into survival to improve
-            the health of people worldwide. We are always on the lookout for
-            dedicated and curious new talents supporting our team of
-            visionaries, entrepreneurs, scientists, engineers and business
-            professionals. Join us and be part of our unique pioneering spirit.
-          </Typography>
+          <Box
+            sx={{
+              position: 'absolute',
+              left: { xl: '-150px', lg: '-100px' },
+              width: '100%',
+            }}>
+            <img
+              style={{
+                objectFit: 'cover',
+                height: '100%',
+                width: '100%',
+              }}
+              src={redbook}
+              alt="pic"
+            />
+          </Box>
+
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '18%',
+              right: '0',
+              background: '#fff',
+              width: '600px',
+              height: '400px',
+              padding: '2pc',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              gap: '1rem',
+            }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                gap: '2rem',
+              }}>
+              <Typography
+                variant="h3"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '600',
+                }}>
+                Careers at Umbrella
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '400',
+                }}>
+                Our mission is clear: translating science into survival to
+                improve the health of people worldwide. We are always on the
+                lookout for dedicated and curious new talents supporting our
+                team of visionaries, entrepreneurs, scientists, engineers and
+                business professionals. Join us and be part of our unique
+                pioneering spirit.
+              </Typography>
+            </Box>
+            <YellowButton>Careers Portal</YellowButton>
+          </Box>
         </Box>
-        <YellowButton>Careers Portal</YellowButton>
-      </Box>
-    </Box>
+      ) : (
+        <Box
+          sx={{
+            mt: '60px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            height: '450px',
+          }}>
+          <Box
+            sx={{
+              border: '1px solid black',
+              background: '#fff',
+              width: '600px',
+              height: '400px',
+              padding: '2pc',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              gap: '1rem',
+            }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                gap: '2rem',
+              }}>
+              <Typography
+                variant="h3"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '600',
+                }}>
+                Careers at Umbrella
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '400',
+                }}>
+                Our mission is clear: translating science into survival to
+                improve the health of people worldwide. We are always on the
+                lookout for dedicated and curious new talents supporting our
+                team of visionaries, entrepreneurs, scientists, engineers and
+                business professionals. Join us and be part of our unique
+                pioneering spirit.
+              </Typography>
+            </Box>
+            <YellowButton>Careers Portal</YellowButton>
+          </Box>
+        </Box>
+      )}
+    </>
   )
 }
 
--- src/components/contentOfHomePage/connectWithUs/ConnectWithUs.jsx
+++ src/components/contentOfHomePage/connectWithUs/ConnectWithUs.jsx
@@ -1,64 +1,125 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useMediaQuery } from '@mui/material'
 import YellowButton from '../yellowButton/YellowButton'
 import chair from '../../../pictures/chair.jpg'
 const ConnectWithUs = () => {
+  const isSmallScreen = useMediaQuery('(min-width: 1200px)')
   return (
-    <Box
-      sx={{
-        position: 'relative',
-        marginTop: '60px',
-        width: '100%',
-        height: '600px',
-      }}>
-      <img
-        style={{
-          objectFit: 'cover',
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          right: '-150px',
-        }}
-        src={chair}
-        alt="pic"
-      />
+    <>
+      {isSmallScreen ? (
+        <Box
+          sx={{
+            position: 'relative',
+            marginTop: '60px',
+            width: '100%',
+            height: '600px',
+          }}>
+          <img
+            style={{
+              objectFit: 'cover',
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+              right: '-150px',
+            }}
+            src={chair}
+            alt="pic"
+          />
 
-      <Box
-        sx={{
-          position: 'absolute',
-          top: '26%',
-          left: '0',
-          background: '#fff',
-          width: '550px',
-          height: '300px',
-          padding: '2pc',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          gap: '1rem',
-        }}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '26%',
+              left: '0',
+              background: '#fff',
+              width: '550px',
+              height: '300px',
+              padding: '2pc',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              gap: '1rem',
+            }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                gap: '2rem',
+              }}>
+              <Typography
+                variant="h3"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '600',
+                }}>
+                Connect with us
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '400',
+                }}>
+                Questions about our products, services or clinical trials? Find
+                the right contact person at our Service Center.
+              </Typography>
+            </Box>
+
+            <YellowButton>Contact Us</YellowButton>
+          </Box>
+        </Box>
+      ) : (
         <Box
           sx={{
             display: 'flex',
-            flexDirection: 'column',
+            alignItems: 'center',
             justifyContent: 'center',
-            gap: '2rem',
+            width: '100%',
+            height: '450px',
           }}>
-          <Typography
-            variant="h3"
-            sx={{ fontFamily: "'Open Sans', sans-serif", fontWeight: '600' }}>
-            Connect with us
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ fontFamily: "'Open Sans', sans-serif", fontWeight: '400' }}>
-            Questions about our products, services or clinical trials? Find the
-            right contact person at our Service Center.
-          </Typography>
-        </Box>
+          <Box
+            sx={{
+              border: '1px solid black',
+              background: '#fff',
+              width: '550px',
+              height: '300px',
+              padding: '2pc',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              gap: '1rem',
+            }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                gap: '2rem',
+              }}>
+              <Typography
+                variant="h3"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '600',
+                }}>
+                Connect with us
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontFamily: "'Open Sans', sans-serif",
+                  fontWeight: '400',
+                }}>
+                Questions about our products, services or clinical trials? Find
+                the right contact person at our Service Center.
+              </Typography>
+            </Box>
 
-        <YellowButton>Contact Us</YellowButton>
-      </Box>
-    </Box>
+            <YellowButton>Contact Us</YellowButton>
+          </Box>
+        </Box>
+      )}
+    </>
   )
 }
 
